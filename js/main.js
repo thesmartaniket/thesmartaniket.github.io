dae62582195bc832c14f18e259f1ad5ea5cd18b0
Aniket Biswas <97422997+thesmartaniket@users.noreply.github.com>
2023-03-06 22:51:00 +0530

    V.0.4
    1. new UI
    2. more efficient
    3. better designs and colours
    4. more additional features

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // This whole project is made by Aniket Biswas | LinkedIn - https://www.linkedin.com/in/thesmartaniket
-// version: V.0.3
+// version: V.0.4
 // includes mobile device support
+// main backend
 
 function toggleMobileMenu(menu){
     menu.classList.toggle('open');
