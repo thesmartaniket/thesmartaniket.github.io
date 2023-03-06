b8d19f6b5a0962176068a4de2abceb8349a7d661
Aniket Biswas <97422997+thesmartaniket@users.noreply.github.com>
2023-03-06 12:50:00 +0530

    Delete index.js

--- js/index.js
+++
@@ -1,7 +0,0 @@
-// This whole project is made by Aniket Biswas | LinkedIn - https://www.linkedin.com/in/thesmartaniket
-// version: V.0.3
-// includes mobile device support
-
-function toggleMobileMenu(menu){
-    menu.classList.toggle('open');
-}
